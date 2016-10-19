48f5cac017f11f88c9fffec169c8bee8cdcfd7ef
andrewjanssen <m.andrew.janssen@gmail.com>
2016-10-19 09:07:47 -0700

    Log bad end_now requests and reply with HTTP 404.

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -1,6 +1,11 @@
 class ImpressionsController < ActionController::Base
   def end_now
-    Impression.find_by(request_hash: params[:request_hash]).end!
-    render nothing: true, status: 200
+    if impression = Impression.find_by(request_hash: params[:request_hash])
+      impression.end!
+      render nothing: true, status: 200
+    else
+      logger.debug "Couldn't find Impression #{params[:request_hash]}."
+      render nothing: true, status: 404
+    end
   end
 end
