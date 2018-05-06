b9b80d246d8fddf0a7777ba73d06e83ed09cb0dd
Andrew Janssen <m.andrew.janssen@gmail.com>
2018-05-06 16:42:38 +0000

    Replace deprecated render call removed from Rails 5.1.

--- app/controllers/impressions_controller.rb
+++ app/controllers/impressions_controller.rb
@@ -2,10 +2,10 @@ class ImpressionsController < ActionController::Base
   def end_now
     if impression = Impression.find_by(request_hash: params[:request_hash])
       impression.end!
-      render nothing: true, status: 200
+      head :ok
     else
       logger.debug "Couldn't find Impression #{params[:request_hash]}."
-      render nothing: true, status: 404
+      head :not_found
     end
   end
 end
